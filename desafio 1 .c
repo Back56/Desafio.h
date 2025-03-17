38128de00687ce0e8802a795d116c7b03cfc79ed
Cleudson Santos <163061995+Back56@users.noreply.github.com>
2025-03-17 14:18:22 -0300

    desafio 1 .c

--- desafio 1 .c
+++ desafio 1 .c
@@ -6,14 +6,18 @@ int main() {
     printf("Insira dois números:\n");
     scanf("%d %d", &a, &b);
     
-    long int diff = a - b;
+    long int diff = (long)a - (long)b;
     
     long int ndiff = (-diff);
     while(diff != 0 && ndiff != 0) {
         diff ++;
         ndiff ++;
-        printf("Números : %ld \t %ld \n", diff, ndiff);
+       // printf("Números : %ld \t %ld \n", diff, ndiff);
     }
-
+    if (diff != 0) {
+        printf("O segundo e maior");
+    } else {
+        printf("O primeiro e maior");
+    }
     return 0;
 }
